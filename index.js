3e5b706fa261ad1cd489de5fb97c80429f097592
luciano <lucho.2012.tandil@gmail.com>
2024-12-26 20:09:57 -0300

    Se agrega nuevo endpoint para comenzar con el reconocimiento de objetos en imagenes

--- index.js
+++ index.js
@@ -33,6 +33,40 @@ app.get('/get_fotos_entrenamiento', async (req, res) => {
     }
 })
 
+app.post('/foto_reconocimiento', async (req, res) => {
+    console.log('/foto_reconocimiento')//, req.body);
+    try {
+        const TS_REQ = new Date()
+
+        const IMG          = req.body.base64
+        const ID_IMG       = uuid.v7()
+        const base64String = IMG.split(',');
+        const extension    = base64String[0].split(':')[1].split(';')[0].replace('/', '.')
+        const fileName     = "./tmp_data/" + ID_IMG + extension
+        const buffer       = Buffer.from(base64String[1], 'base64');
+        fs.writeFileSync(fileName, buffer)
+       
+        const INS_OBJ = {
+            id: uuid.v7(),
+            objeto: req.body.objeto,
+            detalles: req.body.detalles,
+            posicion: JSON.stringify(req.body.posicion),
+            imagen: { id: ID_IMG, extension: extension },
+            ts: TS_REQ,
+            ipv4: req.header('x-forwarded-for') ? req.header('x-forwarded-for') : '-',
+            ipv6: '-',
+            user_agent: req.header('user-agent') ? req.header('user-agent') : '-',
+        }
+
+        console.log(INS_OBJ)
+        
+        return res.status(200).send({ "stat": true });
+    } catch (error) {
+        console.log(error)
+        return res.status(200).send({ "stat": false, error: "Error Interno, reintente luego." });
+    }
+});
+
 app.post('/foto_entrenamiento', async (req, res) => {
     console.log('/foto_entrenamiento')//, req.body);
     try {
